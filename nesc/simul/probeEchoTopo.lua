d1a87b55123183fc2297f81458f573e15af3a72b
Carlos Mattoso <calmattoso@hotmail.com>
2012-06-04 16:07:54 -0300

    (+) ProbeEcho topology

---
+++ nesc/simul/probeEchoTopo.lua
@@ -0,0 +1,27 @@
+local simul = require 'nesc'
+
+for i=0, 6 do
+    _G['n'..i] = simul.app {
+		values = {
+    		Photo_readDone = { i, },
+		},
+        defines = {
+            TOS_NODE_ID = i,
+            TOS_COLLISION = 0,
+		SERVER_ID = 1,
+        },
+        source = assert(io.open'../samples/probeEchoTopo.ceu'):read'*a',
+    }
+end
+
+simul.topology {
+    [n0]  = { n1, },
+    [n1]  = { n0, n2, n4, n6, },
+    [n2]  = { n1, n3, n5},
+    [n3]  = { n2, n4, n6, },
+    [n4]  = { n1, n3, n5, },
+    [n5]  = { n2, n4, n6, },
+    [n6]  = { n1, n3, n5, },
+}
+
+simul.shell()
