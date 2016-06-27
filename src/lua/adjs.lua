03a7b01af84f9193d9c909a7a48e2fd94d31bd8d
Francisco Sant'Anna <francisco.santanna@gmail.com>
2016-06-27 14:40:54 -0300

    (>) fix _If -> If

--- src/lua/adjs.lua
+++ src/lua/adjs.lua
@@ -75,15 +75,25 @@ F = {
 
     -- TODO: "__PRE" because of "continue"
     _If__PRE = function (me)
-        if #me==3 and me[3] then
+        local cnd, t, f_or_more = unpack(me)
+        if #me == 3 then
+            -- if <cond> then <t> else <f> end
+            me.tag = 'If'
+            if f_or_more == false then
+                me[3] = node('Block', me.ln,
+                            node('Stmts', me.ln))
+            end
             return      -- has no "else/if" and has "else" clause
+        else
+            return node('If', me.ln,
+                    cnd,
+                    t,
+                    node('Block', me.ln,
+                        node('Stmts', me.ln,
+                            node('_If', f_or_more.ln,
+                                f_or_more,
+                                select(4,unpack(me))))))
         end
-        local ret = me[#me] or node('Nothing', me.ln)
-        for i=#me-1, 1, -2 do
-            local c, b = me[i-1], me[i]
-            ret = node('If', c.ln, c, b, ret)
-        end
-        return ret
     end,
 
 -------------------------------------------------------------------------------
