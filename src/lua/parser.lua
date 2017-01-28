93197c297e7361d714ef38a8d1830f2d72d5f9bb
fsantanna <francisco.santanna@gmail.com>
2017-01-28 18:23:37 -0200

    (>) optional parenthesis for inlined lua/c

--- src/lua/parser.lua
+++ src/lua/parser.lua
@@ -573,7 +573,8 @@ GG = { [1] = x * V'_Stmts' * V'Y' * (P(-1) + E('end of file'))
     , __luacmp = m.Cmt(V'__luacl' * m.Cb'lua',
                     function (s,i,a,b) return a == b end)
 
-    , __exp = P'@' * V'__Exp'
+    , __exp = P'@' * KK'(' * V'__Exp' * KK')'
+            + P'@' * V'__Exp'
 
 -- VARS, VECTORS, POOLS, VTS, EXTS
 
