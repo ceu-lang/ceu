14f51bee87f05dcc4d51883787722cb35bc84b5a
fsantanna <francisco.santanna@gmail.com>
2017-02-18 16:46:49 -0200

    (>) small simplification

--- src/lua/codes.lua
+++ src/lua/codes.lua
@@ -53,8 +53,7 @@ local function CLEAR (me)
     tceu_evt_range __ceu_range = { _ceu_mem, ]]..me.trails[1]..', '..me.trails[2]..[[ };
     tceu_evt_occ __ceu_occ = { {CEU_INPUT__CLEAR,{NULL}}, (tceu_nseq)(CEU_APP.seq+1),
                                NULL, __ceu_range };
-    tceu_stk __ceu_stk = { 1, _ceu_stk, {_ceu_mem,_ceu_trlK,_ceu_trlK} };
-    ceu_bcast(&__ceu_occ, &__ceu_stk);
+    ceu_bcast(&__ceu_occ, _ceu_stk);
 }
 ]])
 end
