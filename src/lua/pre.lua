f857e3347b1749b652897381f4ac7cd5baa00b2e
fsantanna <francisco.santanna@gmail.com>
2017-06-05 19:43:35 -0300

    (>) cc: protect filenames with spaces with "..."

--- src/lua/pre.lua
+++ src/lua/pre.lua
@@ -1,7 +1,7 @@
 -- "-C":  keep comments (because of nesting)
 -- "-dD": repeat #define's (because of macros used as C functions)
 local f = io.popen(CEU.opts.pre_exe..' -C -dD '..CEU.opts.pre_args..
-            ' '..CEU.opts.pre_input..' -o '..CEU.opts.pre_output..' 2>&1')
+            ' "'..CEU.opts.pre_input..'" -o "'..CEU.opts.pre_output..'" 2>&1')
 local out = f:read'*a'
 ASR(f:close(), out)
 
