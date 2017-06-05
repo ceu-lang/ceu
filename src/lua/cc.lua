c3746b7bc1443eab24482c8d10a9e82adb4ad7e3
fsantanna <francisco.santanna@gmail.com>
2017-06-05 20:05:01 -0300

    (>) cc/pre: protect filenames with spaces with "..."

--- src/lua/cc.lua
+++ src/lua/cc.lua
@@ -1,6 +1,6 @@
 --DBG(CEU.opts.cc_exe..' -xc '..CEU.opts.cc_input..' '..  '-o '..CEU.opts.cc_output..' '..  CEU.opts.cc_args..' 2>&1')
-local cc = CEU.opts.cc_exe..' -xc '..CEU.opts.cc_input..' '..
-            '-o '..CEU.opts.cc_output..' '..
+local cc = CEU.opts.cc_exe..' -xc "'..CEU.opts.cc_input..'" '..
+            '-o "'..CEU.opts.cc_output..'" '..
             CEU.opts.cc_args..' 2>&1'
 local f = io.popen(cc)
 local err = f:read'*a'
