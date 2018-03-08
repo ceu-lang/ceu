97491a169ae8cb626ef3e56a54b87ddfb6c864a8
Francisco Sant'Anna <francisco.santanna@gmail.com>
2018-03-07 21:03:34 -0300

    (-) dead code

--- src/lua/scopes.lua
+++ src/lua/scopes.lua
@@ -1,7 +1,6 @@
 --  NO: big = &&small
 local function check_blk (to_blk, fr_blk)
     local Code = AST.par(fr_blk,'Code')
-    local Stmts = Code and AST.get(Code,'',4,'Block',1,'Stmts',4,'Block',1,'Stmts')
 
     -- changes nested watchings to pars
     local watch = AST.par(fr_blk, 'Watching')
@@ -20,12 +19,6 @@ local function check_blk (to_blk, fr_blk)
     if AST.depth(to_blk) >= AST.depth(fr_blk) then
         assert(ok or AST.is_par(fr_blk,to_blk), 'bug found')
         return true
-    elseif Stmts and (
-                AST.get(Stmts,'',1,'Do', 3,'Block')==fr_blk -- code ... -> ...
-            or
-                AST.get(Stmts,'',1,'Block')==fr_blk         -- code ... -> FOREVER
-            ) then
-        return 'maybe'
     else
         --assert(AST.is_par(to_blk,fr_blk), 'bug found')
         return false
@@ -85,9 +78,6 @@ F = {
                         to_blk = to.info.dcl_obj and to.info.dcl_obj.blk or
                                     to.info.dcl.blk
                         ok = check_blk(to_blk, fr_blk)
-                        if to.info.dcl.id=='_ret' and ok=='maybe' then
-                            ok = false
-                        end
                     end
                 end
             end 
